aca53983298d38072f4b8bf79d47887a8b8e6594
nishaque <nishaque@github.com>
2021-01-08 21:27:14 +0300

    initial

---
+++ include/SharedPtr.hpp
@@ -0,0 +1,130 @@
+// Copyright 2020 Yulia Timoshenko <timoshenkojulie01@gmail.com>
+#ifndef INCLUDE_SHAREDPTR_HPP
+#define INCLUDE_SHAREDPTR_HPP
+
+#include <atomic>
+#include <iostream>
+
+template <typename T>
+class SharedPtr {
+ public:
+  SharedPtr() = default;
+  explicit SharedPtr(T* ptr);
+  SharedPtr(const SharedPtr& r);
+  SharedPtr(SharedPtr&& r) noexcept;
+  ~SharedPtr();
+  auto operator=(const SharedPtr& r) -> SharedPtr&;
+  auto operator=(SharedPtr&& r) noexcept -> SharedPtr&;
+
+  explicit operator bool() const;
+  auto operator*() const -> T&;
+  auto operator->() const -> T*;
+
+  auto get() -> T*;
+  void reset();
+  void reset(T* ptr);
+  void swap(SharedPtr& r);
+  auto use_count() const -> size_t;
+
+ private:
+  T* pointer = nullptr;
+  std::atomic_uint* counter = nullptr;
+};
+
+template <typename T>
+SharedPtr<T>::SharedPtr(T* ptr) {
+  pointer = ptr;
+  counter = new std::atomic_uint;
+  *counter = 1;
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& r) {
+  counter = r.counter;
+  pointer = r.pointer;
+  if (counter) ++*counter;
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(SharedPtr&& r) noexcept {
+  pointer = r.pointer;
+  r.pointer = nullptr;
+  counter = r.counter;
+  r.counter = nullptr;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator=(const SharedPtr& r) -> SharedPtr& {
+  if (&r != this) {
+    this->~SharedPtr();
+    pointer = r.pointer;
+    counter = r.counter;
+    if (counter) ++*counter;
+    return *this;
+  } else
+    return *this;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator=(SharedPtr&& r) noexcept -> SharedPtr& {
+  if (&r != this) {
+    this->~SharedPtr();
+    pointer = r.pointer;
+    counter = r.counter;
+    r.pointer = nullptr;
+    r.counter = nullptr;
+    return *this;
+  } else
+    return *this;
+}
+
+template <typename T>
+SharedPtr<T>::operator bool() const {
+  return (bool)pointer;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator*() const -> T& {
+  return *pointer;
+}
+
+template <typename T>
+auto SharedPtr<T>::operator->() const -> T* {
+  return pointer;
+}
+
+template <typename T>
+auto SharedPtr<T>::get() -> T* {
+  return pointer;
+}
+
+template <typename T>
+void SharedPtr<T>::reset() {
+  *this = std::move(SharedPtr<T>());
+}
+
+template <typename T>
+void SharedPtr<T>::reset(T* r) {
+  *this = std::move(SharedPtr<T>(r));
+}
+
+template <typename T>
+void SharedPtr<T>::swap(SharedPtr<T>& r) {
+  std::swap(pointer, r.pointer);
+  std::swap(counter, r.counter);
+}
+
+template <typename T>
+auto SharedPtr<T>::use_count() const -> size_t {
+  return counter ? (size_t)*counter : 0;
+}
+
+template <typename T>
+SharedPtr<T>::~SharedPtr() {
+  if (counter && !--(*counter)) {
+    delete pointer;
+    delete counter;
+  }
+}
+
+#endif  // INCLUDE_SHAREDPTR_HPP
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,67 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Yulia Timoshenko <timoshenkojulie01@gmail.com>
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <iostream>
+#include <memory>
+
+#include "SharedPtr.hpp"
+
+TEST(Example, EmptyTest) { EXPECT_TRUE(true); }
+
+TEST(SharedPtr, constructorFromPointer) {
+  SharedPtr<int> i{new int{5}};
+  ASSERT_EQ(*i, 5);
+}
+TEST(SharedPtr, constructorFromSelf) {
+  SharedPtr<int> i{new int{5}};
+  SharedPtr<int> j(i);
+  ASSERT_EQ(i.get(), j.get());
+  SharedPtr<int> k(std::move(i));
+  ASSERT_EQ(k.get(), j.get());
+}
+TEST(SharedPtr, defaultConstructor) {
+  SharedPtr<int> i;
+  EXPECT_FALSE(bool(i));
+}
+TEST(SharedPtr, movingOperatorEq) {
+  SharedPtr<int> i{new int{5}};
+  SharedPtr<int> j = std::move(i);
+  ASSERT_EQ(*j, 5);
+}
+TEST(SharedPtr, copyingOperatorEq) {
+  SharedPtr<int> i{new int{5}};
+  SharedPtr<int> j = i;
+  ASSERT_EQ(i.get(), j.get());
+}
+TEST(SharedPtr, reseting) {
+  SharedPtr<int> i{new int{5}};
+  i.reset();
+  ASSERT_EQ(i.get(), nullptr);
+  int *a = new int{5};
+  i.reset(a);
+  ASSERT_EQ(i.get(), a);
+}
+TEST(SharedPtr, countinf) {
+  SharedPtr<int> i{new int{5}};
+  SharedPtr<int> j(i);
+  SharedPtr<int> k = i;
+  ASSERT_EQ(i.use_count(), 3);
+  i.reset();
+  j.reset();
+  k.reset();
+  ASSERT_EQ(i.use_count(), 0);
+}
+TEST(SharedPtr, swapping) {
+  int *a = new int{5};
+  SharedPtr<int> i(a);
+  int *b = new int{7};
+  SharedPtr<int> j(b);
+  i.swap(j);
+  ASSERT_EQ(i.get(), b);
+  ASSERT_EQ(j.get(), a);
+}
+TEST(SharedPtr, operatorArrow) {
+  SharedPtr<std::pair<int, int>> a{new std::pair<int, int>{1, 2}};
+  ASSERT_EQ(a->first, 1);
 }
